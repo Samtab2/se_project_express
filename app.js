365c90f94813b70788edd4b576d2774146890a3d
Sammy Tabally <sammytabally@hotmail.com>
2024-04-17 17:19:34 -0500

    New update for my project

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/wtwr_db').then(() => {
-    console.log('Connected to DB');
-}).catch((e) => {
-    console.error((e) => {
-        console.error(e);
-    });
-});
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/wtwr_db")
+  .then(() => {
+    console.log("Connected to DB");
+  })
+   .catch(console.error);
 
 const app = express();
 
 const { PORT = 3001 } = process.env;
 
- console.log("testing");
+console.log("testing");
 
 app.listen(PORT, () => {
-    console.log('Server is running on port ${PORT}');
+  console.log(`Server is running on port ${PORT}`);
 });
