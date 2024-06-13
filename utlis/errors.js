dd373ebddccc5010d28ae2ab2c9e1fb152d58ce4
Sammy Tabally <sammytabally@hotmail.com>
2024-06-13 14:59:21 -0500

    New update for my project

--- utlis/errors.js
+++ utlis/errors.js
@@ -1,50 +1,7 @@
 const REQUEST_SUCCESSFUL = 200;
 const REQUEST_CREATED = 201;
-const INVALID_DATA = {
-  code: 400,
-  text: {
-    message: "The data is invalid",
-  },
-};
-const NOT_FOUND = {
-  code: 404,
-  text: {
-    message: "Not found",
-  },
-};
-const SERVER_ERROR = {
-  code: 500,
-  text: {
-    message: "Server error please try again later",
-  },
-};
-const CONFLICT = {
-  code: 409,
-  text: {
-    message: "Duplicate key error",
-  },
-};
-const UNAUTHORIZED = {
-  code: 401,
-  text: {
-    message: "Unauthorized",
-  },
-};
-
-const FORBIDDEN = {
-  code: 403,
-  text: {
-    message: "Forbidden",
-  },
-};
 
 module.exports = {
   REQUEST_SUCCESSFUL,
   REQUEST_CREATED,
-  INVALID_DATA,
-  NOT_FOUND,
-  SERVER_ERROR,
-  CONFLICT,
-  UNAUTHORIZED,
-  FORBIDDEN,
 };
